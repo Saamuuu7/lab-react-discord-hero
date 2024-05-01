ebb460253253c7e4e2975a73d2daa7209e90ea4b
Samuel Pérez <samuelperez82pm@gmail.com>
2024-05-01 08:53:45 +0200

    unfinished

---
+++ DiscordHero/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>IMAGINE A PLACE ...</h1>
+      </header>
+      <body>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Aperiam dicta officiis est odit quis asperiores quisquam quas vitae expedita, f
+          acere soluta illum unde consequuntur nihil similique! Nostrum earum sequi excepturi?</p>
+        <button className='Mac'>
+          <a href="#">Download for Mac</a>
+        </button>
+        <button className='Browser'>
+          <a href="#">Open Discord in your Browser</a>
+        </button>
+      </body>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
